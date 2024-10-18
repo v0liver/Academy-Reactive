ebb235877cedbdc2545bacf1d3836da8367c8262
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2024-10-18 17:34:32 +0200

    prova

--- EserciziAcademy/academy/esercizi/Esercizio_1_1.java
+++ EserciziAcademy/academy/esercizi/Esercizio_1_1.java
@@ -1,6 +1,12 @@
 package academy.esercizi;
+
 public class Esercizio_1_1 {
     public static void main(String[] args) {
-        System.out.println("BUON LAVORO!!");
+        String nome = "vito";
+        System.out.println("BUON LAVORO!! cc ");
+        int[] array=new int[3];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
