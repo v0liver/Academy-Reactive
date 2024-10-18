a5ab3634856d1df2041775ba21164298a31932c1
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2024-10-18 17:47:00 +0200

    Revert "prova 2"
    This reverts commit 85728cf0da6749dc651cc13944fed962aea3ddaa.

--- EserciziAcademy/academy/esercizi/Esercizio_1_1.java
+++ EserciziAcademy/academy/esercizi/Esercizio_1_1.java
@@ -3,7 +3,6 @@ package academy.esercizi;
 public class Esercizio_1_1 {
     public static void main(String[] args) {
         String nome = "vito";
-        int prova = 0;
         System.out.println("BUON LAVORO!! cc ");
         int[] array=new int[3];
         for (int i = 0; i < array.length; i++) {
