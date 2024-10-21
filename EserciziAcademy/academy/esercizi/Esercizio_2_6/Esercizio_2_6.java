cc8f285fcd6c624e20f364592991bede96e13b56
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2024-10-21 16:39:12 +0200

    Esercizio 2_6

--- EserciziAcademy/academy/esercizi/Esercizio_2_6/Esercizio_2_6.java
+++ EserciziAcademy/academy/esercizi/Esercizio_2_6/Esercizio_2_6.java
@@ -1,5 +1,18 @@
 package academy.esercizi.Esercizio_2_6;
 
 public class Esercizio_2_6 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int posizioneDaEliminare=4;
+        for (int i = posizioneDaEliminare; i < array.length-1; i++) {
+            array[i]=array[i+1];
+        }
+        array[array.length-1]=0;
 
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]+" ");
+        }
+    }
+            
+            
 }
