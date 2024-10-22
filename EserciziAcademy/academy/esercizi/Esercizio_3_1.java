e436c4e324dad74f1099c10979e6ec6e8321e5eb
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2024-10-22 14:35:05 +0200

    Esercizio finito e ottimizzato

--- EserciziAcademy/academy/esercizi/Esercizio_3_1.java
+++ EserciziAcademy/academy/esercizi/Esercizio_3_1.java
@@ -4,30 +4,69 @@ public class Esercizio_3_1 {
     public static void main(String[] args) {
         int[][] tombola = new int[9][10];
         int contatore = 1;
-        int x = 8;
-        int y = 6;
+        int x = 5;
+        int y = 5;
+        int numero = 45;
+        popolaTombola(tombola);
+        System.out.println("Le coordinate inserite portano al numero:  " + (tombola[x - 1][y - 1]));
+        stampaElementiAdiacenti(x, y, tombola);
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 10; j++) {
-                tombola[i][j] = contatore++;
-                System.out.printf("%2d ", tombola[i][j]);
+
+        stampaElementiAdiacentiDaNumero(numero, tombola);
+    }
+
+    private static void stampaElementiAdiacenti(int x, int y, int[][] tombola) {
+
+
+        if (y > 1) {
+            int sinistra = tombola[x - 1][y - 2];
+            System.out.printf("Sinistra: %d%n", sinistra);
+        }
+        if (y < 10) {
+
+            int destra = tombola[x - 1][y];
+            System.out.printf("Destra: %d%n", destra);
+        }
+        if (x > 1) {
+            int sopra = tombola[x - 2][y - 1];
+            System.out.printf("Sopra: %d%n", sopra);
+        }
+        if (x < 9) {
+            int sotto = tombola[x][y - 1];
+            System.out.printf("Sotto: %d%n", sotto);
+        }
+
+
+    }
+
+    private static void stampaElementiAdiacentiDaNumero(int numero, int[][] tombola) {
+
+        for (int i = 0; i < tombola.length; i++) {
+            for (int j = 0; j < tombola[i].length; j++) {
+                if (tombola[i][j] == numero) {
+                    System.out.printf("Il numero %d si trova alle coordinate (%d, %d)%n", numero, i + 1, j + 1);
+                    stampaElementiAdiacenti(i + 1, j + 1, tombola);
+                    return;
+                }
+            }
+        }
+        System.out.println("Numero non trovato.");
+    }
+
+    private static void popolaTombola(int[][] tombola) {
+        int contatore = 1;
+        for (int righe = 0; righe < 9; righe++) {
+            for (int colonne = 0; colonne < 10; colonne++) {
+                tombola[righe][colonne] = contatore++;
+                System.out.printf("%2d ", tombola[righe][colonne]);
 
             }
             System.out.println();
 
         }
-        System.out.println();
-        System.out.println("Le coordinate inserite portano al numero:  " + (tombola[x - 1][y - 1]));
-        stampaElementi(x, y, tombola);
-    }
-
-    private static void stampaElementi(int x, int y, int[][] tombola) {
-        //int sinistra = tombola[x - 1][y - 1] - 1;//inserisco il -1 alla fine in quanto gli indici delle matrici cominciano da 0 e non da 1
-        int sinistra = tombola[x-1][y-2] ;
-        int destra = tombola[x-1][y];
-        int sopra = tombola[x - 2][y - 1];
-        int sotto = tombola[x][y - 1];
-        System.out.printf("Sinistra: %d%nDestra: %d%nSopra: %d%nSotto: %d%n", sinistra, destra, sopra, sotto);
 
     }
+
+
 }
+
