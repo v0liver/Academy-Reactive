3484650748c6c9c20d0d5a8260bf8e2c974a2b07
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2024-10-28 13:48:16 +0100

    Modifica esercizio 2_6, completamento esercizio 24_1

---
+++ EserciziAcademy/academy/esercizi/Esercizio_24_1.java
@@ -0,0 +1,85 @@
+package academy.esercizi;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Esercizio_24_1 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+
+        int biglie = random.nextInt(91) + 10;
+
+        // Determina se l'utente o il computer gioca per primo
+        boolean turnoUtente = random.nextInt(2) == 0;
+
+        // Modalità intelligente o stupida
+        boolean modalitaIntelligente = random.nextInt(2) == 1;//0 attivata, 1 disattivata
+
+        System.out.println("---------------Gioco di Nim---------------");
+        System.out.println();
+        System.out.println("Il mucchio iniziale contiene " + biglie + " biglie.");
+        System.out.println((turnoUtente ? "L'utente" : "Il computer") + " gioca per primo.");
+        System.out.println(turnoUtente ? "L'utente" : "il computer");
+        System.out.println("Il computer gioca in modalità " + (modalitaIntelligente ? "intelligente." : "stupida."));
+
+        while (biglie > 1) {
+            if (turnoUtente) {
+                // Turno dell'utente
+                int biglieDaPrendere;
+                do {
+                    System.out.println("Nel mucchio ci sono " + biglie + " biglie.");
+                    System.out.print("Quante biglie vuoi prendere? ");
+                    biglieDaPrendere = scanner.nextInt();
+                } while (biglieDaPrendere < 1 || biglieDaPrendere > biglie / 2);
+
+                biglie -= biglieDaPrendere;
+                System.out.println("Hai preso " + biglieDaPrendere + " biglie. Rimangono " + biglie + " biglie.");
+            } else {
+
+                // Turno del computer
+                int biglieDAPrendere;
+                if (modalitaIntelligente) {
+                    biglieDAPrendere = mossaIntelligente(biglie);
+                    if (biglieDAPrendere == -1) {
+                        biglieDAPrendere = random.nextInt(biglie / 2) + 1;
+                    }
+                } else {
+                    biglieDAPrendere = random.nextInt(biglie / 2) + 1;
+                }
+
+                biglie -= biglieDAPrendere;
+                System.out.println("Il computer ha preso " + biglieDAPrendere + " biglie. Rimangono " + biglie + " biglie.");
+            }
+
+            turnoUtente = !turnoUtente; // Cambio giocatore tra un turno e l'altro
+        }
+
+        // Calcolo vincitore
+        if (!turnoUtente) {// Uso "!turnoUtente" al posto di "turnoutente" perchè alla riga 56 cambio il turnoutente per switchare tra utente e computer
+            System.out.println("Il computer ha preso l'ultima biglia. Hai vinto!");
+
+        } else {
+            System.out.println("Hai preso l'ultima biglia. Il computer ha vinto!");
+
+        }
+
+    }
+
+
+    private static int mossaIntelligente(int biglie) {
+        int[] potenze2MenoUno = {3, 7, 15, 31, 63};
+
+        for (int potenza : potenze2MenoUno) {
+            if (biglie > potenza) {
+                return biglie - potenza;
+            }
+        }
+
+        return -1; //  biglie = potenza quindi nessuna mossa intelligente da poter fare
+    }
+
+
+}
--- EserciziAcademy/academy/esercizi/Esercizio_2_6/Esercizio_2_6.java
+++ EserciziAcademy/academy/esercizi/Esercizio_2_6/Esercizio_2_6.java
@@ -2,17 +2,25 @@ package academy.esercizi.Esercizio_2_6;
 
 public class Esercizio_2_6 {
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7,8,9,10};
-        int posizioneDaEliminare=4;
-        for (int i = posizioneDaEliminare; i < array.length-1; i++) {
-            array[i]=array[i+1];
-        }
-        array[array.length-1]=0;
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+        System.out.print("Array originale: ");
+        for (int numero : array) {
+            System.out.print(numero + " ");
+        }
+
+        int posizioneDaEliminare = 4;
+        for (int i = posizioneDaEliminare; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[array.length - 1] = 0;
+
+        System.out.println();
+        System.out.print("Array modificato: ");
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]+" ");
+            System.out.print(array[i] + " ");
         }
     }
-            
-            
+
+
 }
