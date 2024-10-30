e78fb9ad86418018ca23119c906201eb0fe7803e
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2024-10-30 14:56:35 +0100

    legibilità codice

--- EserciziAcademy/academy/esercizi/Esercizio_17_2/Microonde.java
+++ EserciziAcademy/academy/esercizi/Esercizio_17_2/Microonde.java
@@ -3,7 +3,7 @@ package academy.esercizi.Esercizio_17_2;
 public class Microonde {
     public static void main(String[] args) {
         PannelloDiControllo microonde = new PannelloDiControllo();
-        
+
         microonde.piuTrentasecondi();
         microonde.cambiareLivelloPotenza();
         microonde.start();
