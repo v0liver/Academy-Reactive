f57a4cfbbc1559319691d4f3207e9f590f7b85a9
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2024-11-18 08:59:26 +0100

    Formattazione codice

--- EserciziAcademy/academy/esercizi/Esercizio_36bis_1/Agenda.java
+++ EserciziAcademy/academy/esercizi/Esercizio_36bis_1/Agenda.java
@@ -1,24 +1,25 @@
 package academy.esercizi.Esercizio_36bis_1;
 
- public class Agenda implements Comparable<Agenda> {
+public class Agenda implements Comparable<Agenda> {
 
 
+    public enum Giorni {LUNEDI, MARTEDI, MERCOLEDI, GIOVEDI, VENERDI, SABATO, DOMENICA}
 
+    ;
 
-     public enum Giorni {LUNEDI, MARTEDI, MERCOLEDI, GIOVEDI, VENERDI, SABATO, DOMENICA};
-    public enum Impegni{TEATRO, CALCETTO, DIVANO}
+    public enum Impegni {TEATRO, CALCETTO, DIVANO}
+
     Giorni giornoDellaSettimana;
     Impegni impegno;
 
 
-
-     public Agenda(Giorni giornoDellaSettimana, Impegni impegno) {
+    public Agenda(Giorni giornoDellaSettimana, Impegni impegno) {
         this.giornoDellaSettimana = giornoDellaSettimana;
         this.impegno = impegno;
     }
 
-     @Override
-     public int compareTo(Agenda o) {
-         return this.giornoDellaSettimana.compareTo(o.giornoDellaSettimana);
-     }
+    @Override
+    public int compareTo(Agenda o) {
+        return this.giornoDellaSettimana.compareTo(o.giornoDellaSettimana);
+    }
 }
