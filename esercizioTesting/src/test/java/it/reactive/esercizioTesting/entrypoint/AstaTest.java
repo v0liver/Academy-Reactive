db060c4b9853806ae9b88c2ac872afe40dd75d73
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2024-12-03 16:42:32 +0100

    Test corretto ma non completo

--- esercizioTesting/src/test/java/it/reactive/esercizioTesting/entrypoint/AstaTest.java
+++ esercizioTesting/src/test/java/it/reactive/esercizioTesting/entrypoint/AstaTest.java
@@ -13,65 +13,69 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import it.reactive.esercizioTesting.businesslogic.ServiceAsta;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AstaTest {
 
-	@InjectMocks Asta asta;
-	@Mock ServiceAsta serviceAsta;
+    @InjectMocks
+    Asta asta;
+    @Mock
+    ServiceAsta serviceAsta;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+//    @Test
+//    public void test() {
+//        fail("Not yet implemented");
+//    }
 
-	private List<String> partecipanti;
+    private List<String> partecipanti;
 
-	@Before
-	void setUp() {
-		partecipanti = Arrays.asList("Vito", "Giuseppe", "Laura");
-		when(serviceAsta.getPartecipanti()).thenReturn(partecipanti);
-	}
+    @Before
+    public void setUp() {
+        //partecipanti = Arrays.asList("Vito", "Giuseppe", "Laura");
+        partecipanti = new ArrayList<>();
+       // when(serviceAsta.getPartecipanti()).thenReturn(partecipanti);
+    }
 
-	@Test
-	void testAvvia() {
-		String oggettoBandito = "Quadro";
+    @Test
+    public void testAvvia() {
 
+        String oggettoBandito = "Quadro";
 
-		asta.avvia(oggettoBandito);
+        asta.avvia(oggettoBandito);
 
-		verify(serviceAsta).inizializza(partecipanti);
-		verify(serviceAsta).setOggettoBandito(oggettoBandito);
-	}
+        verify(serviceAsta).inizializza(partecipanti);
+        verify(serviceAsta).setOggettoBandito(oggettoBandito);
+    }
 
-	@Test
-	void testRilancia() {
-		String nomeBanditore = "Giuseppe";
-		int valore = 100;
-
-
-		when(serviceAsta.getValoreSessioneAsta()).thenReturn(200);
-
-		int valoreCorrente = asta.rilancia(nomeBanditore, valore);
-
-		verify(serviceAsta).rilancia(nomeBanditore, valore);
-		assertEquals(200, valoreCorrente);
-	}
-
-	@Test
-	void testPassa() {
-		String nomeBanditore = "Vito";
+    @Test
+    public void testRilancia() {
+        String nomeBanditore = "Giuseppe";
+        int valore = 100;
 
 
-		when(serviceAsta.verificaFineAsta()).thenReturn(true);
+        when(serviceAsta.getValoreSessioneAsta()).thenReturn(200);
 
-		boolean fineAsta = asta.passa(nomeBanditore);
+        int valoreCorrente = asta.rilancia(nomeBanditore, valore);
 
-		verify(serviceAsta).rilancia(nomeBanditore, -1);
-		assertTrue(fineAsta);
-	}
+        verify(serviceAsta).rilancia(nomeBanditore, valore);
+        assertEquals(200, valoreCorrente);
+    }
+
+    @Test
+    public void testPassa() {
+        String nomeBanditore = "Vito";
+
+
+        when(serviceAsta.verificaFineAsta()).thenReturn(true);
+
+        boolean fineAsta = asta.passa(nomeBanditore);
+
+        verify(serviceAsta).rilancia(nomeBanditore, -1);
+        assertTrue(fineAsta);
+    }
 
 
 }
