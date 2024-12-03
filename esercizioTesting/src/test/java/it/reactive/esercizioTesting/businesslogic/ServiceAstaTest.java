837d5d1ad3cc041055496edd52ad3138a06ff88f
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2024-12-03 16:06:51 +0100

    Test con mockito non completo

--- esercizioTesting/src/test/java/it/reactive/esercizioTesting/businesslogic/ServiceAstaTest.java
+++ esercizioTesting/src/test/java/it/reactive/esercizioTesting/businesslogic/ServiceAstaTest.java
@@ -1,6 +1,10 @@
 package it.reactive.esercizioTesting.businesslogic;
 
 import it.reactive.esercizioTesting.entrypoint.Asta;
+import it.reactive.esercizioTesting.exception.AstaTerminataException;
+import it.reactive.esercizioTesting.exception.PartecipanteNonCensitoException;
+import it.reactive.esercizioTesting.exception.TurnoNonValidoException;
+import it.reactive.esercizioTesting.exception.ValoreNonAmmessoException;
 import it.reactive.esercizioTesting.oggetto.SessioneAsta;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,10 +17,9 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class ServiceAstaTest {
-    static ServiceAsta serviceAsta;
+    static ServiceAsta serviceAsta=new ServiceAsta();
     static Asta asta;
-
-    List<String> partecipanti = new ArrayList<>(Arrays.asList("Vito","Giuseppe","Laura"));
+    List<String> partecipanti = new ArrayList<>(Arrays.asList("Vito", "Giuseppe", "Laura"));
 
 
     @Before
@@ -27,27 +30,52 @@ public class ServiceAstaTest {
     }
 
 
-        @Test
+    @Test
     public void inizializza() {
-        assertEquals(partecipanti, serviceAsta.getPartecipanti());
+        // assertEquals(partecipanti, serviceAsta.getPartecipanti());
 //        assertTrue(serviceAsta.isInCorso());
 //        assertEquals(new ArrayList<String>(),sessioneAsta.getPartecipanti());
-
-    }
-    @Test
-    public void testRilanciaValorePositivo() {
-        serviceAsta.rilancia("Partecipante1", 10);
-        assertEquals(10, serviceAsta.getValoreSessioneAsta());
-        assertEquals("Partecipante1", serviceAsta.getVincitore());
     }
 
 
+//    @Test(expected = AstaTerminataException.class)
+//    public void rilanciaConAstaTerminataException() {
+//
+//    }
     @Test
-    public void setOggettoBandito() {
+    public void setOggetto(){
+        serviceAsta.setOggettoBandito("ciao");
     }
 
-    @Test
-    public void rilancia() {
+    @Test(expected = PartecipanteNonCensitoException.class)
+    public void rilanciaPartecipanteNonCensitoException() {
+        serviceAsta.rilancia("Daniele", 5);
+    }
+
+    @Test(expected = TurnoNonValidoException.class)
+    public void rilanciaTurnoNonValidoException() {
+        serviceAsta.rilancia("Giuseppe", 10);
+    }
+
+    @Test(expected = ValoreNonAmmessoException.class)
+    public void rilanciaValoreNonAmmessoException() {
+        serviceAsta.rilancia("Vito", 0);
+    }
+
+    @Test(expected = AstaTerminataException.class)
+    public void rilanciaAstaTerminataException() {
+        List<String> partecipanti = new ArrayList<>();
+        partecipanti.add("Daniele");
+        serviceAsta.inizializza(partecipanti);
+        serviceAsta.rilancia("Vito", 5);
+    }
+
+    public void rilanciaAstaTerminataException() {
+//        List<String> partecipanti = new ArrayList<>();
+//        partecipanti.add("Daniele");
+//        serviceAsta.inizializza(partecipanti);
+//        serviceAsta.rilancia("Vito", 5);
+        serviceAsta.rilancia("Vito",-1);
     }
 
     @Test
--- esercizioTesting/src/test/java/it/reactive/esercizioTesting/entrypoint/AstaTest.java
+++ esercizioTesting/src/test/java/it/reactive/esercizioTesting/entrypoint/AstaTest.java
@@ -1,7 +1,10 @@
 package it.reactive.esercizioTesting.entrypoint;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -9,6 +12,9 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import it.reactive.esercizioTesting.businesslogic.ServiceAsta;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AstaTest {
@@ -20,5 +26,52 @@ public class AstaTest {
 	public void test() {
 		fail("Not yet implemented");
 	}
-	
+
+	private List<String> partecipanti;
+
+	@Before
+	void setUp() {
+		partecipanti = Arrays.asList("Vito", "Giuseppe", "Laura");
+		when(serviceAsta.getPartecipanti()).thenReturn(partecipanti);
+	}
+
+	@Test
+	void testAvvia() {
+		String oggettoBandito = "Quadro";
+
+
+		asta.avvia(oggettoBandito);
+
+		verify(serviceAsta).inizializza(partecipanti);
+		verify(serviceAsta).setOggettoBandito(oggettoBandito);
+	}
+
+	@Test
+	void testRilancia() {
+		String nomeBanditore = "Giuseppe";
+		int valore = 100;
+
+		
+		when(serviceAsta.getValoreSessioneAsta()).thenReturn(200);
+
+		int valoreCorrente = asta.rilancia(nomeBanditore, valore);
+
+		verify(serviceAsta).rilancia(nomeBanditore, valore);
+		assertEquals(200, valoreCorrente);
+	}
+
+	@Test
+	void testPassa() {
+		String nomeBanditore = "Vito";
+
+
+		when(serviceAsta.verificaFineAsta()).thenReturn(true);
+
+		boolean fineAsta = asta.passa(nomeBanditore);
+
+		verify(serviceAsta).rilancia(nomeBanditore, -1);
+		assertTrue(fineAsta);
+	}
+
+
 }
