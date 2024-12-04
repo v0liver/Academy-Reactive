2e91404c98621c667d971a5a4fa2e878065b01e6
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2024-12-04 17:51:24 +0100

    Esercizio Testing completato con 100% di coverage con jacoco sia Asta che Service asta

--- EserciziAcademy/academy/esercizi/Esercizio_38_1/TestGiocoDelSogno.java
+++ EserciziAcademy/academy/esercizi/Esercizio_38_1/TestGiocoDelSogno.java
@@ -5,35 +5,10 @@ import java.util.Scanner;
 
 public class TestGiocoDelSogno {
     public static void main(String[] args) {
-
+    ElementoCasualeOptional ciao = new ElementoCasualeOptional();
 
     }
     private void soluzione() {
-        Scanner scanner = new Scanner(System.in);
-        GiocoDelSognoOptional giocoDelSegnoOptional = new GiocoDelSognoOptional();
-        boolean fineGioco = false;
-        Optional<Integer> valoreUtente;
-        int numeroMassimo = 2000;
 
-        System.out.println("Benvenuto nel Gioco del Segno!");
-        System.out.println("Cerca di indovinare il valore! Puoi inserire 'null' per un valore assente.");
-        do {
-            System.out.println("Inserisci un numero da 1 ad un massimo di " + numeroMassimo + " o null per un valore assente:");
-            String controlloNull = scanner.nextLine();
-            numeroMassimo = numeroMassimo/2;
-
-            if (controlloNull.equals("null")) {
-                valoreUtente = Optional.empty();
-            } else {
-                try {
-                    valoreUtente = Optional.of(Integer.parseInt(controlloNull));
-                } catch (NumberFormatException e) {
-                    System.out.println("Input non valido. Per favore, inserisci un numero valido o 'null'.");
-                    continue;
-                }
-            }
-            fineGioco = giocoDelSegnoOptional.gioco(valoreUtente);
-
-        } while (!fineGioco);
     }
 }
--- esercizioTesting/src/test/java/it/reactive/esercizioTesting/businesslogic/ServiceAstaTest.java
+++ esercizioTesting/src/test/java/it/reactive/esercizioTesting/businesslogic/ServiceAstaTest.java
@@ -1,12 +1,7 @@
 package it.reactive.esercizioTesting.businesslogic;
 
 import it.reactive.esercizioTesting.entrypoint.Asta;
-import it.reactive.esercizioTesting.exception.AstaTerminataException;
-import it.reactive.esercizioTesting.exception.PartecipanteNonCensitoException;
-import it.reactive.esercizioTesting.exception.TurnoNonValidoException;
-import it.reactive.esercizioTesting.exception.ValoreNonAmmessoException;
-import it.reactive.esercizioTesting.oggetto.SessioneAsta;
-import org.junit.Assert;
+import it.reactive.esercizioTesting.exception.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,16 +9,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ServiceAstaTest {
-    static ServiceAsta serviceAsta=new ServiceAsta();
+    static ServiceAsta serviceAsta = new ServiceAsta();
     static Asta asta;
     List<String> partecipanti = new ArrayList<>(Arrays.asList("Vito", "Giuseppe", "Laura"));
 
 
     @Before
-    public void before() {
+    public void setUp() {
         serviceAsta = new ServiceAsta();
         asta = new Asta(serviceAsta);
         serviceAsta.inizializza(partecipanti);
@@ -31,20 +26,9 @@ public class ServiceAstaTest {
 
 
     @Test
-    public void inizializza() {
-        // assertEquals(partecipanti, serviceAsta.getPartecipanti());
-//        assertTrue(serviceAsta.isInCorso());
-//        assertEquals(new ArrayList<String>(),sessioneAsta.getPartecipanti());
-    }
-
-
-//    @Test(expected = AstaTerminataException.class)
-//    public void rilanciaConAstaTerminataException() {
-//
-//    }
-    @Test
-    public void setOggetto(){
+    public void setOggetto() {
         serviceAsta.setOggettoBandito("ciao");
+        //Non posso controllare il metodo con junit perchè non esiste il getOggettoBandito
     }
 
     @Test(expected = PartecipanteNonCensitoException.class)
@@ -70,29 +54,82 @@ public class ServiceAstaTest {
         serviceAsta.rilancia("Vito", 5);
     }
 
-//    public void rilanciaAstaTerminataException() {
-////        List<String> partecipanti = new ArrayList<>();
-////        partecipanti.add("Daniele");
-////        serviceAsta.inizializza(partecipanti);
-////        serviceAsta.rilancia("Vito", 5);
-//        serviceAsta.rilancia("Vito",-1);
-//    }
+    @Test
+    public void rilanciaValoreMaggioreZero() {
+        serviceAsta.rilancia("Vito", 10);
+        assertEquals("Vito", serviceAsta.getVincitore());
+    }
+
+    @Test
+    public void rilanciaValoreMinoreZero() {
+        serviceAsta.rilancia("Vito", -10);
+
+        //Non posso controllare altro con junit in quanto non posso riprendere i valori di
+        // sessioneAsta.getPartecipantiPassati().add(nomePartecipante);
+    }
+
+    @Test
+    public void rilanciaProgressivoUgualePartecipantiSize() {
+        List<String> tmp = new ArrayList<>(Arrays.asList("Vito", "Laura"));
+        serviceAsta.inizializza(tmp);
+        serviceAsta.rilancia("Vito", 10);
+        serviceAsta.rilancia("Laura", 20);
+
+
+        //Non posso controllare altro con junit in quanto non posso controllare
+        // se effettivamente progressivoTurnoCorrente==0
+    }
+
+    @Test
+    public void rilancia() {
+        serviceAsta.rilancia("Vito", -10);
+        serviceAsta.rilancia("Giuseppe", 20);
+        serviceAsta.rilancia("Laura", 40);
+        serviceAsta.rilancia("Giuseppe", 50);
+
+
+    }
 
     @Test
     public void getValoreSessioneAsta() {
+        assertEquals(1, serviceAsta.getValoreSessioneAsta());
     }
 
     @Test
     public void getVincitore() {
+        //serviceAsta.getVincitore();
+        assertEquals("Vito", serviceAsta.getVincitore());
+
     }
 
-    @Test
+    @Test(expected = UnsupportedOperationException.class)
     public void setPartecipanti() {
+        serviceAsta.setPartecipanti(partecipanti);
+    }
+
+    @Test(expected = AstaInCorsoException.class)
+    public void addPartecipanteAstaInCorsoException() {
+        serviceAsta.addPartecipante("Santina");
+
+    }
+
+    @Test(expected = PartecipanteEsistenteException.class)
+    public void addPartecipantePartecipanteEsistenteException() {
+        serviceAsta.fine();
+        serviceAsta.addPartecipante("Vito");
+
+
     }
 
     @Test
-    public void addPartecipante() {
+    public void addPartecipantePartecipante() {
+        serviceAsta.fine();
+        serviceAsta.addPartecipante("Santina");
+        assertEquals("Santina", partecipanti.get(3));
+
+
     }
+
 
     @Test
     public void getPartecipanti() {
