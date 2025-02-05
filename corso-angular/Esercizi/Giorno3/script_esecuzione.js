c662e7cef9dd6848fca79e0fdfea16284b4cd338
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2025-02-05 20:44:42 +0100

    Esercizio Giorno3

---
+++ corso-angular/Esercizi/Giorno3/script_esecuzione.js
@@ -0,0 +1,55 @@
+function getParagrafs() {
+    let cont = 0;
+    let listaParagrafi = document.getElementsByTagName("p");
+
+    // listaParagrafi.forEach(element => {
+    //     element.innerHTML="ciao";
+    // });
+
+    for (let i = 0; i < listaParagrafi.length; i++) {
+        listaParagrafi[i].innerHTML = "blocco " + (++cont);
+    }
+}
+
+function greetEveryone() {
+    alert("Ciao a Tutti");
+}
+
+function showTime(id) {
+
+    const time = new Date();
+    const ore = time.getHours();
+    const minuti = time.getMinutes();
+    const secondi = time.getSeconds();
+
+    document.getElementById(id).textContent = `${ore}:${minuti}:${secondi}`;
+
+}
+
+function setIntervalloClock(id, millisecondi) {
+    setInterval(() => showTime(id, millisecondi));
+}
+
+function changeBackground(className) {
+    document.getElementsByClassName(className)[0].style.backgroundColor = 'black';
+}
+
+function validazioneDati(event) {
+    event.preventDefault(); // Evita l'invio del form
+
+    
+    const nome = document.getElementById('nome').value;
+    const cognome = document.getElementById('cognome').value;
+    const data_nascita = document.getElementById('data').value;
+    const genere = document.getElementById('genere').value;
+    const email = document.getElementById('email').value;
+
+    console.log(genere);
+    console.log(nome);
+    
+  }
+
+  document.getElementById('formMyhero').addEventListener("submit", validazioneDati);
+
+
+
