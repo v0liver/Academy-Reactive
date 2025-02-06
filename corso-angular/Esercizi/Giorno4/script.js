4151d75fbc4dcc80b953fa85dd2eb8a1a258fad1
ITMASTER\V.Oliveri-cons <V.Oliveri-cons@reactive-almaviva.it>
2025-02-06 13:50:22 +0100

    upd

--- corso-angular/Esercizi/Giorno4/script.js
+++ corso-angular/Esercizi/Giorno4/script.js
@@ -1,40 +1,40 @@
 let array = [1, 2, 2, 10, 20, 15, 20, 75];
 
-let json=[
+let json = [
     {
-      "nome": "Mario",
-      "cognome": "Rossi",
-      "eta": 30,
-      "data_di_nascita": "1994-03-15"
+        "nome": "Mario",
+        "cognome": "Rossi",
+        "eta": 30,
+        "data_di_nascita": "1994-03-15"
     },
     {
-      "nome": "Anna",
-      "cognome": "Bianchi",
-      "eta": 25,
-      "data_di_nascita": "1999-07-22"
+        "nome": "Anna",
+        "cognome": "Bianchi",
+        "eta": 25,
+        "data_di_nascita": "1999-07-22"
     },
     {
-      "nome": "Luca",
-      "cognome": "Verdi",
-      "eta": 35,
-      "data_di_nascita": "1989-11-05"
+        "nome": "Luca",
+        "cognome": "Verdi",
+        "eta": 35,
+        "data_di_nascita": "1989-11-05"
     },
     {
-      "nome": "Giulia",
-      "cognome": "Neri",
-      "eta": 28,
-      "data_di_nascita": "1996-01-10"
+        "nome": "Giulia",
+        "cognome": "Neri",
+        "eta": 28,
+        "data_di_nascita": "1996-01-10"
     },
     {
-      "nome": "Marco",
-      "cognome": "Gialli",
-      "eta": 40,
-      "data_di_nascita": "1984-08-17"
+        "nome": "Marco",
+        "cognome": "Gialli",
+        "eta": 40,
+        "data_di_nascita": "1984-08-17"
     }
-  ]
-;
+]
+    ;
 
-let stringJson=`[
+let stringJson = `[
   {
     "nome": "Mario",
     "cognome": "Rossi",
@@ -109,27 +109,26 @@ function removeDuplicates() {
 }
 
 function filterByCriteria() {
-    const result = array.filter(element=>element>10);
+    const result = array.filter(element => element > 10);
     console.log(result);
 }
 
 
-function jsonStrToObj(){
+function jsonStrToObj() {
     const jsonFromString = JSON.parse(stringJson);
     console.log(jsonFromString);
-    
+
 }
 
-function jsonObjToStr(){
-    const stringFromJson= JSON.stringify(json);
+function jsonObjToStr() {
+    const stringFromJson = JSON.stringify(json);
     console.log(stringFromJson);
 }
 
 function manipulate(obj, property, value) {
     obj[property] = value;
-    console.log(obj); 
-  }
+    console.log(obj);
+}
 
-  
- 
-  
+
+
